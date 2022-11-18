eccc3617ea5ba67d13c4ff5fce431ce6633d8bb7
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-11-18 21:11:41 +0000

    working tcl and tests

--- polaris_irule.tcl
+++ polaris_irule.tcl
@@ -1,8 +1,7 @@
 rule polaris {
   when HTTP_REQUEST {
-    if { [HTTP::uri] starts_with "/polaris" } {
-      HTTP::redirect "https://polaris.int.cps.gov.uk/init?cookie="[HTTP::header "cookie"]
-    } 
+    if { [HTTP::uri] eq "/polaris" } {
+      HTTP::redirect [format "https://polaris.int.cps.gov.uk/init?cookie=%s&referer=%s" [URI::encode [HTTP::header "cookie"]] [HTTP::header "referer"]]
+    }
   }
 }
-
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 # Experiment with big ip rules combined with [TestTcl](https://github.com/landro/TesTcl)
+[![CI](https://github.com/chrisns/bigip-irule-demo/actions/workflows/ci.yaml/badge.svg)](https://github.com/chrisns/bigip-irule-demo/actions/workflows/ci.yaml)
 
+> Simple proof of concept using tcl to define a rule with TCL for a F5 BIG-IP appliance with tests
